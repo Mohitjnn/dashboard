eba2d49234370cbaf337019608856a11ef807120
Mohitjnn <jnmohit29@gmail.com>
2024-06-28 02:45:03 +0530

    section below navbar made

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import MyComponent from "@/components/Filters/Filters";
 
 export default function Home() {
   return (
     <main>
-      <center></center>
+      <center>
+        <section
+          className="w-full flex justify-start px-20 items-center space-x-20 h-[10vh]"
+          style={{ boxShadow: "0px 0px 2px  #897d7d" }}
+        >
+          <MyComponent />
+        </section>
+      </center>
     </main>
   );
 }
---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useState } from "react";
+
+const filterArray = ["Job Preview", "Applicants", "Match", "Messages"];
+
+const MyComponent = () => {
+  const [activeIndex, setActiveIndex] = useState(-1);
+
+  const handleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <>
+      {filterArray.map((filter, index) => (
+        <button
+          key={index}
+          className={`text ${
+            index === activeIndex
+              ? "text-orange-500 border-b-2 border-orange-500"
+              : "text-gray-500"
+          } h-full content-center`}
+          onClick={() => handleClick(index)}
+        >
+          {filter}
+        </button>
+      ))}
+    </>
+  );
+};
+
+export default MyComponent;
